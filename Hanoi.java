a2e718175ad910353d6432b18703cdc0d0cc09fa
Camilo Martinez <camiloaww@gmail.com>
2017-11-14 15:30:44 -0500

    Arreglo hanoi.java

--- Hanoi.java
+++ Hanoi.java
@@ -1,12 +1,15 @@
-package proyecto_final;
+
 import acm.program.ConsoleProgram;
 
 public class Hanoi extends ConsoleProgram{
+	int [] tini = {};
+	int [] taux = {};
+	int [] tfin = {};
 
 	public void run() {
 		//prueba del método
-		torresH(3, 'a','b', 'c');
-		println("El numero de pasos fueron: " + nPasos(3));
+		torresH(8, 'A','B', 'C');
+		println("El numero de pasos fueron: " + nPasos(8));
 		
 	}
 	/**
@@ -18,10 +21,10 @@ public class Hanoi extends ConsoleProgram{
 	 */
 	public void torresH(int numd,char torrei,char torreaux, char torref) {
 		if (numd == 1) {
-			println("Mueva disco de " + torrei + " a " + torref);
+			println("Mueva disco "+numd+ " de " + torrei + " a " + torref);
 		} else {
 			torresH(numd - 1, torrei, torref, torreaux);
-			println("Mueva disco de " + torrei + " a " + torref);
+			println("Mueva disco "+numd+" de " + torrei + " a " + torref);
 			torresH(numd - 1, torreaux, torrei, torref);
 		}
 	}
--- Interfaz1.java
+++ Interfaz1.java
@@ -7,10 +7,17 @@ public class Interfaz1 extends GraphicsProgram {
 	private int numDisk;
 	private int sizeDisk;
 	private GRoundRect d;
-	private GRect e;
 	
+	public static final int hDisco = 10;
+	
+	public static final int bDisco = 20;
+	
+	public static final double cAstaX = 225;
+	
+	public static final int cAstaY= 290;
+		
 	public void run() {
-		discosApilados(8);
+		discosApilados(3);
 		astas();
 	}
 	/**
@@ -20,8 +27,8 @@ public class Interfaz1 extends GraphicsProgram {
 	 */
 	public GRoundRect discosC (int g) {
 		numDisk = g;
-		sizeDisk = numDisk*20;
-		d = new GRoundRect(sizeDisk,10);
+		sizeDisk = numDisk*bDisco;
+		d = new GRoundRect(sizeDisk, hDisco);
 		return d;
 	}
 	
@@ -30,17 +37,27 @@ public class Interfaz1 extends GraphicsProgram {
 	 * @param n Número de discos en la pila
 	 */
 	public void discosApilados(int n) {
-		for (int i = 1;i<=n; i++) {
+		for (int i = n ; i>=0; i--) {
 			d = discosC(i);
-			add(d, 100+(n*10 - i*10), 100+(10*i));
-		}
+			d.setFilled(true);
+			d.setFillColor(Color.RED);
+			add(d, cAstaX - 10*i, cAstaY);
+			d.move(0, -10);
+		}	
 	}
 	public void astas () {
-		e = new GRect (5, 200);
+		GRect e = new GRect (125-2.5,0,5, 200);
 		e.setFilled(true);
 		e.setFillColor(Color.GRAY);
-		add(e);
+		GRect b = new GRect (0,200,240,10);
+		b.setFilled(true);
+		b.setFillColor(Color.GRAY);
+		GCompound ast = new GCompound();
+		ast.add(e);
+		ast.add(b);
+		add(ast,100,100);
+		ast.sendToBack();
 	}
 	
-
+///*100-2.5+(k*10)*/
 }
